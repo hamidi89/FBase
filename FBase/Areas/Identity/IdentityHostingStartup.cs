b2a085a7920dea6ffbb5d7186834a212b38a8817
Hamidi <nohammadehamidi@gmail.com>
2020-08-15 19:23:49 +0430

    Add project files.

---
+++ FBase/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FBase.Areas.Identity.Data;
+using FBase.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FBase.Areas.Identity.IdentityHostingStartup))]
+namespace FBase.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FBaseContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FBaseContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FBaseContext>();
+            });
+        }
+    }
+}
---
+++ FBase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
